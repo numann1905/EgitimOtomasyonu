708da20fce5ce83efa3c6c37e72400b5198a86b7
numann1905 <numann1905@users.noreply.github.com>
2026-05-12 15:34:30 +0300

    Initial education automation project

---
+++ veritabani/__init__.py
@@ -0,0 +1,6 @@
+# veritabani/__init__.py
+# Bu dosya klasörü bir Python paketi haline getirir.
+# Gerekirse burada ortak bağlantı fonksiyonları da tanımlanabilir.
+
+from .veritabani_olustur import veritabani_olustur
+from .admin_ekle import admin_ekle
